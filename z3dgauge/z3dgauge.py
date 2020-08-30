90fa62910b36e74260ac48d8ea8ad3301385ad5b
iocube <52827727+iocube@users.noreply.github.com>
2020-08-30 21:15:02 +0300

    Add files via upload
    Laid down some groundwork for the actual 3D part:
    - managed to render my first primitive (wooo!)
    - added acceleration variable

--- z3dgauge/z3dgauge.py
+++ z3dgauge/z3dgauge.py
@@ -1,7 +1,6 @@
 ﻿#importing the thingies
 
 import ac, acsys
-import sys
 
 
 import os
@@ -37,12 +36,13 @@ def acMain(ac_version): #app window init; global variables for later updating go
     # gonna need new textures like
     # rpm_bar = ac.newTexture(app_path + theme_path + "rpm_bar.png")
 
-    global l_kmph, l_rpm, l_gear
+    global l_kmph, l_rpm, l_gear, acceleration
     global ascii_RPM
     l_kmph = ac.addLabel(appWindow, "KMPH")
     l_rpm = ac.addLabel(appWindow, "RPM")
     l_gear = ac.addLabel(appWindow, "Current gear")
     ascii_RPM = ac.addLabel(appWindow, "")
+    acceleration = ac.addLabel(appWindow, "")
     #l_TC = ac.addLabel(appWindow, "TC on/off")      #non-functional
     #l_ABS = ac.addLabel(appWindow, "ABS on/off")    #non-functional
 
@@ -50,20 +50,37 @@ def acMain(ac_version): #app window init; global variables for later updating go
     ac.setPosition(l_rpm, 3, 60)
     ac.setPosition(l_gear, 3, 80)
     ac.setPosition(ascii_RPM, 3, 160)
+    ac.setPosition(acceleration, 3, 180)
 
     ac.addRenderCallback(appWindow, appGL) # -> links this app's window to an OpenGL render function
-
 
     return appName
 
 def appGL(deltaT):
-    pass #delete to enable
+    
+
+    x0 = 50
+    y0 = 50
+    x1 = 50
+    y1 = 300
+    x2 = 200
+    y2 = 350
+    x3 = 250
+    y3 = 50  # so I'm gonna have to find a way to make these variables be affected by gforces
+    ac.glBegin(acsys.GL.Quads)
+    ac.glVertex2f(5, 5) # top left in brainlet terms aka for dummies like me
+    ac.glVertex2f(5, 20)   # bottom left
+    ac.glVertex2f(20, 30) # bottom right
+    ac.glVertex2f(20, 5)   #top right
+    ac.glEnd()
+
+    
 
 def acUpdate(deltaT):
 
     ac.setBackgroundOpacity(appWindow, 0)
     ac.drawBorder(appWindow, 0)
-    global l_kmph, l_rpm, l_gear, speed, RPM, gear
+    global l_kmph, l_rpm, l_gear, speed, RPM, gear, acceleration
 
     speed = str(ac.getCarState(0, acsys.CS.SpeedKMH))
     RPM = str(ac.getCarState(0, acsys.CS.RPM))
@@ -72,6 +89,7 @@ def acUpdate(deltaT):
     ac.setText(l_kmph, speed)
     ac.setText(l_rpm, RPM)
     ac.setText(l_gear, gear)
+    
 
     global ascii_RPM, ARPM
 
@@ -84,6 +102,9 @@ def acUpdate(deltaT):
         ARPM = ARPM + "[!]"
     
     ac.setText(ascii_RPM, ARPM)
+    accelerationS = str(ac.getCarState(0, acsys.CS.AccG)) # returns a tuple with x y z; so X is sideways, Y and Z idk - to find out!
+    ac.setText(acceleration, accelerationS)
+    print(acceleration)
 
 
 def tacho(anotherRPM):      #idk wtf is going on anymore; thanks to based serb for the function though
